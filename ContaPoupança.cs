c6943e6876a6eab306232f03a4440171601bd9e8
thales eduardo <36962603+xThaleSxX@users.noreply.github.com>
2018-09-24 22:13:44 -0300

    Add files via upload

---
+++ ContaPoupança.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class ContaPoupanca
+    {
+        static double taxaDeJurosAnual = 0;
+        private double saldoPoupanca = 0;
+
+        public ContaPoupanca(double saldoPoupanca) 
+        {
+            this.saldoPoupanca = saldoPoupanca;
+        }
+
+        public static void AlterarTaxaDeJuros(double novaTaxaDeJurosAnual)
+        {
+            taxaDeJurosAnual = novaTaxaDeJurosAnual;
+        }
+
+        public double CalcularJuroMensal()
+        {
+            saldoPoupanca += (saldoPoupanca * taxaDeJurosAnual) / 12;
+            return saldoPoupanca;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaPoupanca poupador1 = new ContaPoupanca(2000);
+            ContaPoupanca poupador2 = new ContaPoupanca(3000);
+
+            ContaPoupanca.AlterarTaxaDeJuros(0.04);
+
+            Console.Write("*Juros de 4%\n\n");
+
+            Console.Write("Novo Saldo do poupador1: R$ " + poupador1.CalcularJuroMensal());
+
+            Console.Write("\nNovo Saldo do poupador2: R$ " + poupador2.CalcularJuroMensal());
+
+            ContaPoupanca.AlterarTaxaDeJuros(0.05);
+
+            Console.Write("\n\n\n*Juros de 5%\n\n");
+
+            Console.Write("Novo Saldo do poupador1: R$ " + poupador1.CalcularJuroMensal());
+
+            Console.Write("\nNovo Saldo do poupador2: R$ " + poupador2.CalcularJuroMensal());
+
+            Console.ReadKey();
+        }
+    }
+}
